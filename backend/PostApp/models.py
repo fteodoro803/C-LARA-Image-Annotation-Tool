91dd3c60d77552a07ee25ae707d25e70ab1a3aa4
fteodoro803 <agustinteodoro803@gmail.com>
2023-09-23 12:42:03 +1000

    Updated Database Models

--- backend/PostApp/models.py
+++ backend/PostApp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-# JSON File to be sent to FrontEnd
+# JSON File per Image to be sent to FrontEnd
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    image = models.ImageField(upload_to='images')
+    imageName = models.CharField(max_length=100, primary_key=True)
+    imageLocation = models.ImageField(upload_to='images')
+    words = models.TextField()    #~note I want this to be a List, and to have each word have a corresponding Coordinate List
     coordinates = models.JSONField(default=list)
 
     def __str__(self):
-        return self.title
+        return self.imageName
