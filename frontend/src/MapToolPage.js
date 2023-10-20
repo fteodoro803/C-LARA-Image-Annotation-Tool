15a2532675eb00051d4f47e32dd3b626c1e01aaf
jahnavi0011 <141203262+jahnavi0011@users.noreply.github.com>
2023-10-20 17:48:16 +1100

    Updated MapTool with more functions and fixed the undo and redo issues

--- frontend/src/MapToolPage.js
+++ frontend/src/MapToolPage.js
@@ -22,23 +22,6 @@ function MapToolPage({ onBackClick }) {
         // Implement done logic here
     }
 
-    const handleUndo = () => {
-        if (actionStack.length > 0) {
-            const lastAction = actionStack.pop();
-            undoStack.push(lastAction);
-            setActionStack([...actionStack]);
-            setUndoStack([...undoStack]);
-        }
-    }
-    
-    const handleRedo = () => {
-        if (undoStack.length > 0) {
-            const lastUndoneAction = undoStack.pop();
-            actionStack.push(lastUndoneAction);
-            setActionStack([...actionStack]);
-            setUndoStack([...undoStack]);
-        }
-    }
     
 
     const canvasRef = useRef(null);
@@ -46,63 +29,207 @@ function MapToolPage({ onBackClick }) {
 
     const [isDrawing, setIsDrawing] = useState(false);
     const [tool, setTool] = useState('');  // '', 'pencil', or 'eraser'
+    const [penColor, setPenColor] = useState('black');
     const [lines, setLines] = useState([]);
+    const [showPaletteDropdown, setShowPaletteDropdown] = useState(false);
+    const colors = ['black', 'red', 'white','teal'];
+    const dropdownRef = useRef(null);
+
+   useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowPaletteDropdown(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     useEffect(() => {
-        const canvas = imageCanvasRef.current;
-        const context = canvas.getContext('2d');
+        const context = imageCanvasRef.current.getContext('2d');
+        drawImageToCanvas(context);
+    }, [selectedImage]);
+
+    useEffect(() => {
+        redrawCanvas(actionStack, canvasRef.current.getContext('2d'));
+    }, [actionStack]);
+
+    const drawImageToCanvas = (context) => {
         const image = new Image();
         image.src = selectedImage;
         image.onload = () => {
-            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+            context.drawImage(image, 0, 0, context.canvas.width, context.canvas.height);
         }
-    }, [selectedImage]);
+
+    }
+
+    
     
 
     const startDrawing = (e) => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        context.beginPath();
-        context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
         setIsDrawing(true);
-    }
+        const context = canvasRef.current.getContext('2d');
+    
+        if (tool === 'pencil') {
+            context.globalCompositeOperation = 'source-over';
+            context.strokeStyle = penColor;
+            context.lineWidth = 2;
+        } else if (tool === 'eraser') {
+            context.globalCompositeOperation = 'destination-out';
+            context.lineWidth = 10;
+        }
+    
+        context.beginPath();  // Start a new path for the drawing or erasing
+        setLines([{ x: e.clientX - canvasRef.current.offsetLeft, y: e.clientY - canvasRef.current.offsetTop }]);
+    };
 
     const draw = (e) => {
         if (!isDrawing || !tool) return;
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
+    
+        // Setup context properties based on the current tool
         if (tool === 'pencil') {
-            context.lineWidth = 2;
-            context.strokeStyle = 'black';
-            context.lineCap = 'round';
             context.globalCompositeOperation = 'source-over';
+            context.strokeStyle = penColor;
+            context.lineWidth = 2;
         } else if (tool === 'eraser') {
             context.globalCompositeOperation = 'destination-out';
             context.lineWidth = 10;
         }
-        context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    
+        const x = e.clientX - canvas.offsetLeft;
+        const y = e.clientY - canvas.offsetTop;
+    
+        setLines(prev => [...prev, { x, y }]);  // Store line coordinates
+    
+        context.lineTo(x, y);
         context.stroke();
-    }
+    };
     
 
     const stopDrawing = () => {
         setIsDrawing(false);
-        if (tool === 'pencil') {
-            const canvas = canvasRef.current;
-            const context = canvas.getContext('2d');
-            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-            setLines([...lines, imageData]);
+    
+        // Check if the start and end points are close
+        const startPoint = lines[0];
+        const endPoint = lines[lines.length - 1];
+        const distance = Math.sqrt(Math.pow(endPoint.x - startPoint.x, 2) + Math.pow(endPoint.y - startPoint.y, 2));
+    
+        if (distance > 10) {  // A threshold, you can adjust this value as needed
+            alert("Annotation not complete! Please connect boundary end points.");
+            return;  // Do not save the current annotation
         }
-    }
+    
+        setActionStack(prev => [
+            ...prev,
+            {
+                type: tool,
+                path: lines,
+                color: tool === 'pencil' ? penColor : null,
+                width: tool === 'pencil' ? 2 :10
+            }
+        ]);
+        
+        setLines([]);
+    };
+
+    const redrawCanvas = (actions, canvasContext) => {
+        canvasContext.clearRect(0,0,canvasContext.canvas.width, canvasContext.canvas.height);
+        
+        drawImageToCanvas(canvasContext);
+        
+        actions.forEach (action=> {
+            canvasContext.beginPath();
+            canvasContext.strokeStyle = action.color;
+            canvasContext.lineWidth = action.width;
+            canvasContext.globalCompositeOperation = action.type === 'eraser' ? 'destination-out' : 'source-over';
+            
+            const [firstPoint, ...restOfPoints] = action.path;
+            canvasContext.moveTo(firstPoint.x, firstPoint.y);
+
+            restOfPoints.forEach(point => {
+                canvasContext.lineTo(point.x, point.y);
+                canvasContext.stroke();
+            });
+
+            canvasContext.closePath();
+
+        });
+        
+
+    };
+
+    const handleClearAll = () => {
+        const context = canvasRef.current.getContext('2d');
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        drawImageToCanvas(context);
+
+        if (actionStack.length > 0)
+        {
+            setUndoStack([...undoStack, ...actionStack]);
+        }
+        setActionStack([]);
+      
+    };
 
     const handleEraser = () => {
         setTool('eraser');
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        context.putImageData(lines[lines.length - 1], 0, 0);
     }
 
+    const lastActionTimeRef = useRef(null);
 
+    // Helper function to handle debouncing
+    const isDebounced = (delay = 300) => {
+        const currentTime = Date.now();
+        if (lastActionTimeRef.current && currentTime - lastActionTimeRef.current < delay) {
+            return true;
+        }
+        lastActionTimeRef.current = currentTime;
+        return false;
+    };
+
+
+    
+
+
+    const handleUndo = () => {
+        if (isDebounced()) return;  // Avoid rapid successive clicks
+
+        console.log("Before Undo:", actionStack, undoStack);
+        
+        if (actionStack.length === 0) return;
+
+        const newUndoAction = actionStack[actionStack.length - 1];
+        setUndoStack(prev => [...prev, newUndoAction]);
+
+        setActionStack(prev => {
+            const newActionStack = prev.slice(0, -1);
+            return newActionStack;
+        });
+
+        console.log("After Undo:", actionStack, undoStack);
+    };
+
+    const handleRedo = () => {
+        if (isDebounced()) return;  // Avoid rapid successive clicks
+
+        console.log("Before Redo:", actionStack, undoStack);
+        
+        if (undoStack.length === 0) return;
+
+        const redoAction = undoStack[undoStack.length - 1];
+        setUndoStack(prev => prev.slice(0, -1));
+
+        setActionStack(prev => [...prev, redoAction]);
+
+        console.log("After Redo:", actionStack, undoStack);
+    };
+    
+    
     const handleBackClick = () => {
         const userConfirmed = window.confirm("Do you want to save your progress?");
         if (userConfirmed) {
@@ -112,6 +239,9 @@ function MapToolPage({ onBackClick }) {
             navigate("/imagedetail");
         }
     };
+
+    
+    
     
 
     return (
@@ -123,9 +253,19 @@ function MapToolPage({ onBackClick }) {
                 <button onClick={handleUndo}>⟲</button>
                 <button onClick={handleRedo}>⟳</button>
                 <button onClick={() => setTool('pencil')}>✏️</button>
+                {colors.map(color => (
+                  <button 
+                    key={color} 
+                    className={`color-button ${color}`} 
+                    style={{ backgroundColor: color }} 
+                    onClick={() => setPenColor(color)}
+                  >
+                  </button>
+                ))}
                 <button onClick={handleEraser}>🧽</button>
-            </div>
+                <button onClick={handleClearAll}>🗑️ Clear All</button>
 
+            </div>
             <div className="image-container">
     <canvas 
         ref={canvasRef} 
@@ -143,7 +283,6 @@ function MapToolPage({ onBackClick }) {
         style={{ position: 'relative', zIndex: 0 }}
     />
 </div>
-
 
             <div className="word-choice">
                 <p>Choose words:</p>
