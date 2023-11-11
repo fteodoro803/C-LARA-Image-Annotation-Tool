b52273a4f271596a7d320823737b37ce1c0d0137
fteodoro803 <agustinteodoro803@gmail.com>
2023-11-12 00:53:13 +1100

    renamed django app to MapTool

--- backend/MapTool/apps.py
+++ backend/MapTool/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PostAppConfig(AppConfig):
+class MapToolConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PostApp'
+    name = 'MapTool'
